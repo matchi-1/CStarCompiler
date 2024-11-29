4421f1e34c1ff95b5c6685ba63a822e11cf20b1b
Michelle Tolentino <misheruvynx@gmail.com>
2024-11-30 00:42:15 +0800

    i forgor what i added but its there in frontend somewhere

--- frontend/src/components/Terminal.jsx
+++ frontend/src/components/Terminal.jsx
@@ -6,7 +6,7 @@ const Terminal = ({ logs }) => {
     <div className="terminal">
       <div className="tab-containers">
         <div className="tab-item">
-            <p>compiler logs</p>
+            <p>Compiler Logs</p>
         </div>
         <div className="tab-filler"></div>
       </div>
