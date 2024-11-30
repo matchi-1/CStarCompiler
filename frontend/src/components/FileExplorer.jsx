5e9f1f858e3c5b7d444f09553350057e8f1c28b9
l-uci-d <s.abenoja04@gmail.com>
2024-11-30 17:29:50 +0800

    non-working firebase upload thingy idk man

--- frontend/src/components/FileExplorer.jsx
+++ frontend/src/components/FileExplorer.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { FaFolder, FaFile } from 'react-icons/fa';
-import { initializeApp } from "firebase/app";
-import { IoIosArrowBack } from 'react-icons/io'; // Icon for the collapse button
-import '../styles/FileExplorer.css'; // Import the CSS file
+import { IoIosArrowBack } from 'react-icons/io'; 
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+import { getDatabase } from 'firebase/database';
+import '../styles/FileExplorer.css'; 
 
-const FileExplorer = ({ isVisible, files, addFile, addFolder, toggleFiles }) => {
+const FileExplorer = ({ isVisible, files, addFolder, toggleFiles, handleFileUpload }) => {
+  const [uploading, setUploading] = useState(false);
+  const fileInputRef = useRef(null); 
+  const handleAddFileClick = () => {
+      fileInputRef.current.click(); 
+    };
+
   return (
     <div className={`file-explorer ${isVisible ? 'visible' : ''}`}>
-      {/* Collapse button */}
       <button className="collapse-button" onClick={toggleFiles}>
         <IoIosArrowBack />
       </button>
 
       <div className="file-explorer-header">
         <h3>File Explorer</h3>
-        <button onClick={addFile}>Add File</button>
+       
+        <button onClick={handleAddFileClick}>Add File</button>
         <button onClick={addFolder}>Add Folder</button>
       </div>
+
+
+      <div className="file-upload">
+        <input
+          ref={fileInputRef}  
+          type="file"
+          accept=".cstr"  
+          onChange={handleFileUpload}  
+          style={{ display: 'none' }}  
+        />
+      </div>
+
       <div className="file-explorer-content">
         {files.length === 0 ? (
           <p>No files or folders available</p>
