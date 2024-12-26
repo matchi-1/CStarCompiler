4a0384479a60c7a8f2656f20537c2ad801359770
Stephen Abenoja <118324984+l-uci-d@users.noreply.github.com>
2024-12-26 14:21:32 +0800

    pr please (#37)
    * added
    
    * Added workflow to sync fork
    
    * Update main.yml
    
    * Update main.yml
    
    * added dockerfiles
    
    * updated package-lock thingy idk man
    
    * added 'fixes from defense'
    
    * added "fixes"

--- backend/app.py
+++ backend/app.py
@@ -3098,4 +3098,4 @@ def compile_code():
     return jsonify(lexres)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000) 
