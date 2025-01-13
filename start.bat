39090b58f53b805afa529b83cedb2b2c6858256c
alex <122758355+alexong04@users.noreply.github.com>
2025-01-14 02:11:14 +0800

    returned orig bat

--- start.bat
+++ start.bat
@@ -1,10 +1,6 @@
 @echo off
-
-cd "C:\Users\ariar\Desktop\CStarCompiler\backend"
+cd /d "./backend"
 start cmd /k "python app.py"
-
-cd "C:\Users\ariar\Desktop\CStarCompiler\frontend"
+cd /d "../frontend"
 start cmd /k "npm start"
-
-
 pause
