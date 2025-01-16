bdff8e7a7566ff0af15c2120e79e288ab44fa13c
l-uci-d <s.abenoja04@gmail.com>
2025-01-16 12:52:42 +0800

    revised and added boilerplate, finished import, added initial var_dec and function_dec

--- backend/syntax_analyzer.py
+++ backend/syntax_analyzer.py
@@ -1,4 +1,10 @@
-#---PARSER---
+#-------------------- PREDICT SETS --------------------
+PREDICT_imports_rec = ["import", "private", "class", "int", "long", "bool", "float", "double", "string", "const", "Identifier"]
+PREDICT_imports_rec_values = ["Cmath", "Cstring", "Carray"]
+
+DATATYPES = ["bool", "string", "int", "long", "double", "float"]
+
+#-------------------- PARSER --------------------
 class SyntaxAnalyzer:
     # Takes tokens, initializes current token and its index
     def __init__(self, tokens):
@@ -17,6 +23,16 @@ class SyntaxAnalyzer:
 
         self.lineContent = ''
 
+    #-------------------- PARSER START --------------------
+    def parse(self):
+        #print(self.tokens)
+        try:
+            self.program()
+            print("Parsing completed successfully.")
+        except SyntaxError as e:
+            print(f"Parsing incomplete with error/s: {e}")
+
+    #-------------------- HELPER FUNCTIONS --------------------
     # Advancer for the next token
     def nextToken(self):
         self.currToken_index += 1
@@ -26,57 +42,159 @@ class SyntaxAnalyzer:
         else:
             self.currToken = None
 
-    # If current token type matches the expected token type, advances to the next token
-    def terminal(self, expected_type):
-        if self.currToken is not None and self.currToken["tokenType"] == expected_type: ##TODO
+    # Checks if given token matches the expected token type, advances to the next token, else raises error
+    def match(self, expected_token):
+        if self.currToken is not None and self.currToken["tokenType"] == expected_token: ##TODO
             self.nextToken()
+        else: 
+
+            if self.currToken is None:  # EOF
+                self.raiseError(expected_token, "Unexpected EOF")
+
+            else: # Wrong token
+                self.raiseError(expected_token, "Unexpected token")
+
+    def matchPredictSet(self, expected_predict_set):
+        if self.currToken is None:  # EOF
+            self.raiseError("", "Unexpected EOF", expected_predict_set)
+        if self.currToken["tokenType"] not in expected_predict_set:
+            self.raiseError("", "Unexpected token", expected_predict_set)
+            
+
+    #-------------------- SYNTAX ERRORS --------------------
+    # Current Syntax Errors:    
+    #   - Unexpected EOF
+    #   - Unexpected token
+    #
+
+    def raiseError(self, expected_token, error_type, expected_predict_set=[]):
+        if not self.currToken:
+            currToken = self.tokens[self.currToken_index - 1]
+            currLine = currToken["tokenLine"]
+            currCol = currToken["tokenCol"]
+        else: 
+            currToken = self.currToken["tokenName"]
+            currLine = self.currToken["tokenLine"]
+            currCol = self.currToken["tokenCol"]
+
+        # Determine the expected message
+        if expected_predict_set:
+            # Format the list of expected tokens
+            expected_tokens = ", ".join(f"'{token}'" for token in expected_predict_set)
+            expected_message = f"{expected_tokens}"
         else:
-            # Incomplete Token Error
-            if self.currToken is None: self.unexpectedEnd(expected_type)
-            # Unexpected Token Error
-            else: self.unexpectedToken(expected_type)
+            expected_message = f"'{expected_token}'"
 
-    # SYNTAX ERRORS
-    # !!!! TODO: add pointers to errors 
-    def unexpectedEnd(self, expected_type):
-        errorType = "Unexpected end"
-        currToken = self.tokens[self.currToken_index - 1]
-        currLine = currToken["tokenLine"]
-        currCol = currToken["tokenCol"]
-        message = f"\n\tSyntax Error: {errorType} at line {currLine}, column {currCol}" + "\n\tExpected: '{}'\n".format(expected_type)
+        # Construct the error message
+        message = (
+            f"\n\tSyntax Error: {error_type}"
+            + (f" '{currToken}'" if self.currToken else "")
+            + f" at line {currLine}, column {currCol}"
+            f"\n\tExpected: {expected_message}\n"
+        )
         self.errors.append(message)
-        raise SyntaxError(message)
-
-    def unexpectedToken(self, expected_type):
-        errorType = "Unexpected token"
-        currToken = self.currToken["tokenName"]
-        currLine = self.currToken["tokenLine"]
-        currCol = self.currToken["tokenCol"]
-        message = f"\n\tSyntax Error: {errorType} '{currToken}' at line {currLine}, column {currCol}" + "\n\tExpected: '{}'\n".format(expected_type)
-        self.errors.append(message) 
         raise SyntaxError(message)
         # lineContent = TBC LOL I'm thinking of extracting the line from the code using currCol and line[0] to line[currLine]
         # return generateError(errorType, currToken, currLine, currCol)
 
-    # PARSER
-    def parse(self):
-        #print(self.tokens)
-        try:
-            self.program()
-            print("Parsing completed successfully.")
-        except SyntaxError as e:
-            print(f"Parsing incomplete with error/s: {e}")
 
-    # BARE-MINIMUM ONLY for testing, will be edited during finalization
+    #-------------------- CFG START --------------------
     def program(self):
-        """<program> → int main(){return 0;}"""
-        self.terminal("int")
-        self.terminal("Identifier")
-        self.terminal("(")
-        self.terminal(")")
-        self.terminal("{")
-        self.terminal("return")
-        self.terminal("whole_lit")
-        self.terminal(";")
-        self.terminal("}")
         print("(parser) production: \"program\" detected")
+        """<program> → <imports_list><program_constructs> int main(){ <main_body> return 0;}"""
+        self.imports_list()
+        self.matchPredictSet(PREDICT_imports_rec)
+        self.program_constructs()
+        self.match("int")
+        if self.currToken and self.currToken["tokenName"] == "main":
+            self.match("Identifier")
+            self.match("(")
+            self.match(")")
+            self.match("{")
+            # self.main_body()
+            self.match("return")
+            self.match("whole_lit")
+            self.match(";")
+            self.match("}")
+        else:
+            self.raiseError("main", "Unexpected Token")
+       
+    def imports_list(self):
+        print("(parser) production: \"imports_list\" detected")
+        """<imports_list> → import <iostar>;<imports_rec>"""
+        self.match("import")
+        self.match("<")
+        if self.currToken and self.currToken["tokenName"] == "iostar":
+            self.match("Identifier")  # iostar
+        self.match(">")
+        self.match(";")
+        self.matchPredictSet(PREDICT_imports_rec)
+        if self.currToken and self.currToken["tokenType"] == "import":
+            self.imports_rec()
+
+    def imports_rec(self):
+        print("(parser) production: \"imports_rec\" detected")
+        """<imports_rec> → import <<imports_rec_values>>;<imports_rec> | λ"""
+        self.match("import")
+        self.match("<")
+        self.imports_rec_values()
+        self.match(">")
+        self.match(";")
+        self.matchPredictSet(PREDICT_imports_rec)
+        if self.currToken and self.currToken["tokenType"] == "import":
+            self.imports_rec()
+
+    def imports_rec_values(self):
+        print("(parser) production: \"imports_rec_values\" detected")
+        if self.currToken["tokenName"] in PREDICT_imports_rec_values:
+            self.std_lib()
+        elif self.currToken and self.currToken["tokenType"] == "Identifier":
+            self.match("Identifier")
+            self.match(".")
+            if self.currToken and self.currToken["tokenName"] == "cstr":
+                self.match("Identifier")
+            else:
+                self.raiseError("cstr file", "Unexpected Token")
+        else:
+            self.raiseError("", "Unexpected Token", PREDICT_imports_rec_values + ["or cstr file"])
+
+    def std_lib(self):
+        print("(parser) production: \"std_lib\" detected")
+        """<std_lib> → Cmath | Cstring | Carray"""
+        if self.currToken["tokenName"] in {"Cmath", "Cstring", "Carray"}:
+            self.nextToken()
+
+    def program_constructs(self):
+        print("(parser) production: \"program_constructs\" detected")
+        """<program_constructs> → <class_declaration><program_constructs> | <function_dec><program_constructs> | <var_dec><program_constructs> | <class_inst><program_constructs> | λ"""
+        if self.currToken["tokenType"] == "private" or self.currToken["tokenType"] == "class":
+            self.class_declaration()
+        elif self.currToken["tokenType"] == "const":
+            self.var_dec()
+        else:
+            self.matchPredictSet(DATATYPES)
+            self.nextToken
+            self.match("Identifier")
+            if self.currToken["tokenType"] == "(":
+                self.function_dec()
+            elif self.currToken["tokenType"] == "=":
+                self.var_dec()
+
+        
+
+    def class_declaration(self):
+        print("(parser) production: \"class_declaration\" detected")
+        if self.currToken["tokenType"] == "private":
+            self.match("private")
+        self.match("class")
+        self.match("Identifier")
+        self.match("{")
+        #self.class_body()
+        self.match("}")
+        self.match(";")
+
+    def var_dec(self):
+        print("(parser) production: \"var_dec\" detected")
+
+    def function_dec(self):
+        print("(parser) production: \"function_dec\" detected")
