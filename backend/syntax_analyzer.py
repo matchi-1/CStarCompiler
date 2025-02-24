b72d48b85759d72b1a45982825abc3eb7d354d77
alex <122758355+alexong04@users.noreply.github.com>
2025-02-24 22:43:07 +0800

    no tokens error

--- backend/syntax_analyzer.py
+++ backend/syntax_analyzer.py
@@ -212,13 +212,13 @@ class SyntaxAnalyzer:
             if token.token_type != ("single_comment" or "multi-line comment")] # comments will be ignored by the parser
         # print(self.tokens) #uncomment to check tokens that the parser accepted
         
-        if not self.tokens:
-            message = "\n\tNo tokens to parse."
-            self.errors.append(message)
-            raise SyntaxError(message)
+        #if not self.tokens:
+        #    message = "\n\tNo tokens to parse."
+        #    self.errors.append(message)
+        #    raise SyntaxError(message)
 
         self.currToken_index = 0
-        self.currToken = self.tokens[self.currToken_index]
+        self.currToken = self.tokens[self.currToken_index] if self.tokens else None
 
         self.lineContent = ''
         self.hasMainFunction = False  # Track if main function is found
@@ -495,6 +495,11 @@ class SyntaxAnalyzer:
     def program(self):
         print("(parser) production: \"program\" detected")
         """<program> → <imports_list><program_constructs> int main(){ <main_body> return 0;}"""
+        if not self.tokens:
+            message = "\n\tNo tokens to parse."
+            self.errors.append(message)
+            raise SyntaxError(message)
+        
         if self.matchPredictSet("program", False):
             self.imports_list()
             
