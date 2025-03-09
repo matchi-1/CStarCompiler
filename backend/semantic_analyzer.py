66b8ffd1448956e9a2d2f93057cb8bf746241a40
alex <122758355+alexong04@users.noreply.github.com>
2025-03-09 19:52:24 +0800

    semantic: fixes to support exp eval

--- backend/semantic_analyzer.py
+++ backend/semantic_analyzer.py
@@ -977,42 +977,46 @@ class SemanticAnalyzer:
             idec_rec = node.vardec_cont_n.idec_rec_n
                     
         print(f" -------------------------------------------> val_type: {val_type[1]} d_type: {dtype[1]}")
-        if val_type and dtype[1] != val_type[1]:
-            match(dtype[1]):
-                case "int":
-                    if val_type[1] not in ["string", "bool"]:
-                        if value > self.MAX_INT or value < self.MIN_INT:
-                            self.logError(f"Value '{value}' is out of 'int' range for variable '{id}'.", node.id_n)
-                     
-                    self.logError(f"Type Mismatch: expected '{dtype[1]}' for variable '{id}' but found '{val_type[1]}'.", node.id_n)   
         
-                case "long":
-                    if val_type[1] not in ["string", "bool"]:
-                        if value > self.MAX_LONG or value < self.MIN_LONG:
-                            self.logError(f"Value '{value}' is out of 'long' range for variable '{id}'.", node.id_n)
-                    
+        match(dtype[1]):
+            case "int":
+                if val_type[1] not in ["string", "bool"]:
+                    if value > self.MAX_INT or value < self.MIN_INT:
+                        self.logError(f"Value '{value}' is out of 'int' range for variable '{id}'.", node.id_n)
+                
+                if val_type and dtype[1] != val_type[1]:    
+                    self.logError(f"Type Mismatch: expected '{dtype[1]}' for variable '{id}' but found '{val_type[1]}'.", node.id_n)   
+    
+            case "long":
+                if val_type[1] not in ["string", "bool"]:
+                    if value > self.MAX_LONG or value < self.MIN_LONG:
+                        self.logError(f"Value '{value}' is out of 'long' range for variable '{id}'.", node.id_n)
+                
+                if val_type and dtype[1] != val_type[1]:
                     if val_type[1] != "int":
                         self.logError(f"Type Mismatch: expected '{dtype[1]}' for variable '{id}' but found '{val_type[1]}'.", node.id_n)
-        
-                case "float":
-                    if val_type[1] not in ["string", "bool"]:
-                        if value > self.MAX_FLOAT or value < self.MIN_FLOAT:
-                            self.logError(f"Value '{value}' is out of 'float' range for variable '{id}'.", node.id_n)
-                    
+    
+            case "float":
+                if val_type[1] not in ["string", "bool"]:
+                    if value > self.MAX_FLOAT or value < self.MIN_FLOAT:
+                        self.logError(f"Value '{value}' is out of 'float' range for variable '{id}'.", node.id_n)
+                
+                if val_type and dtype[1] != val_type[1]:
                     if val_type[1] != "int":
                         self.logError(f"Type Mismatch: expected '{dtype[1]}' for variable '{id}' but found '{val_type[1]}'.", node.id_n)
 
-                case "double":
-                    if val_type[1] not in ["string", "bool"]:
-                        if value > self.MAX_DOUBLE or value < self.MIN_DOUBLE:
-                            self.logError(f"Value '{value}' is out of 'double' range for variable '{id}'.", node.id_n)
-                    
+            case "double":
+                if val_type[1] not in ["string", "bool"]:
+                    if value > self.MAX_DOUBLE or value < self.MIN_DOUBLE:
+                        self.logError(f"Value '{value}' is out of 'double' range for variable '{id}'.", node.id_n)
+                
+                if val_type and dtype[1] != val_type[1]:
                     if val_type[1] not in ["int", "float", "long"]:
                         self.logError(f"Type Mismatch: expected '{dtype[1]}' for variable '{id}' but found '{val_type[1]}'.", node.id_n)
 
-                case _:
-                    if dtype[1] != val_type[1]: 
-                        self.logError(f"Type Mismatch: expected '{dtype[1]}' for variable '{id}' but found '{val_type[1]}'.", node.id_n)   
+            case _:
+                if val_type and dtype[1] != val_type[1]:
+                    self.logError(f"Type Mismatch: expected '{dtype[1]}' for variable '{id}' but found '{val_type[1]}'.", node.id_n)   
 
         if not value:
             match dtype[1]:
