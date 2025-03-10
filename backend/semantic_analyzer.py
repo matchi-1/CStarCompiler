8a0c9133363207f56f2294cba32a4409df36fb04
gekiiMei <gekimeshi@gmail.com>
2025-03-11 01:00:44 +0800

    func decarr placeholder sizes

--- backend/semantic_analyzer.py
+++ backend/semantic_analyzer.py
@@ -695,7 +695,7 @@ class SemanticAnalyzer:
                 elif type(param).__name__ == "node_funcpar_arr":
                     arr_dtype = ('arr', param.dtype_t["tokenName"]) if param.dtype_t else None  # for any types -- std lib Carray
                     arr_dim = param.arrdim_i if param.arrdim_i else None   # for any dimensions -- std lib Carray
-                    self.curr_scope.set_array(param_name, value=[self.default_vals[arr_dtype[1]]], dtype=arr_dtype, arr_info={"dimension": arr_dim}, const=False)
+                    self.curr_scope.set_array(param_name, value=[self.default_vals[arr_dtype[1]]], dtype=arr_dtype, arr_info={"dimension": arr_dim, "size1": 1, "size2": 2 if arr_dim == 2 else None}, const=False)
 
                 elif type(param).__name__ == "node_funcpar_var":
                     var_dtype = ('var', param.dtype_t["tokenName"])
