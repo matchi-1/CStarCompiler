a048c7c107df2d356c57e56586eb94b7da1bf452
gekiiMei <gekimeshi@gmail.com>
2025-03-11 18:39:50 +0800

    object value priviliges restored

--- backend/semantic_analyzer.py
+++ backend/semantic_analyzer.py
@@ -575,9 +575,7 @@ class SemanticAnalyzer:
                     self.logError(f"Symbol {node.id_t["tokenName"]} is a function and needs to be called.", node)
                 case 'class':
                     self.logError(f"Symbol {node.id_t["tokenName"]} is a class and needs to be instantiated.", node)
-                case 'object':
-                    self.logError(f"Symbol {node.id_t["tokenName"]} is an object and cannot be used as a value.", node)
-                    
+
             print(f'RETURNED FROM NODE_IDEN: iden_symbol["dtype"]: {iden_symbol.get("dtype", None)}, iden_symbol["value"]:{iden_symbol.get("value", None)}')
             return (iden_symbol.get("dtype", None), iden_symbol.get("value", None))
             # return (('var', iden_symbol["dtype"][1]), None)
