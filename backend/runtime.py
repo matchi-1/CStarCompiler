0dc1f321e44c44cdb66c61f7d9a95c92f6113c70
alex <122758355+alexong04@users.noreply.github.com>
2025-04-07 00:38:08 +0800

    output: compound assign eval

--- backend/runtime.py
+++ backend/runtime.py
@@ -847,6 +847,7 @@ class Runtime:
         value = node.value_n
         iden_name = iden.id_t["tokenName"]
         iden_symbol = self.curr_scope.get(iden_name)
+        assign_op = node.op_t["tokenName"]
 
         id_err = ErrorNode(iden.id_t["tokenLine"], iden.id_t["tokenCol"] - len(iden.id_t["tokenName"]) - 1)
         if not iden_symbol: self.logError(f"Symbol '{iden_name}' hasn't been declared yet.", id_err)
@@ -870,13 +871,36 @@ class Runtime:
         self.check_type_and_range("variable", iden_symbol["dtype"], val_type, val, node.id_n, err_n = val_err)
         
         self.curr_scope.set(iden_name, val, dtype=('var', f'{dtype}'))
-
+        check_scope = self.curr_scope
+        while not check_scope.get(iden_name, False):
+            check_scope = check_scope.parent
+        
+        match assign_op:
+            case "=":
+                check_scope.syms[iden_name]["value"] = val
+            case "+=":
+                check_scope.syms[iden_name]["value"] += val
+            case "-=":
+                check_scope.syms[iden_name]["value"] -= val
+            case "*=":
+                check_scope.syms[iden_name]["value"] *= val
+            case "/=":
+                check_scope.syms[iden_name]["value"] /= val
+            case "%=":
+                if val == 0:
+                    self.logError("Modulo by 0 is not allowed.", val_err)
+                if iden_symbol["dtype"][1] not in ["int", "long"] or val_type[1] not in ["int", "long"]:
+                    self.logError("Type mismatch for arithmetic expression, modulo operation only supports whole numbers (int, long)", val_err)
+                if iden_symbol["dtype"][1] == "int" and val_type[1] == "long":
+                    self.logError(f"Type mismatch for arithmetic expression, expected numeric value (int, long) for both operands, but got {iden_symbol["dtype"]} and {val_type}.", val_err)
+                check_scope.syms[iden_name]["value"] %= val
 
     def visit_node_assign_stmt_array_elem(self, node): 
         # visit_node_assign_stmt_object_att_arr REFERENCES THIS, CHANGE BOTH FUNCS WHEN U CHANGE THIS ONE THANK U
         arr_node = node.id_arr_n   # current node
         arr_name = arr_node.id_n.id_t["tokenName"]
         arr_symbol = self.curr_scope.get(arr_name)  # reference node in sym table
+        assign_op = node.op_t["tokenName"]
 
         if not arr_symbol:
             self.logError(f"Array '{arr_name}' hasn't been declared yet.", arr_node.id_n)
@@ -933,7 +957,49 @@ class Runtime:
         #     arr_symbol["value"][idx1_val] = value
         # else:
         #     arr_symbol["value"][idx1_val][idx2_val] = value
-        
+        check_scope = self.curr_scope
+        while not check_scope.get(arr_name, False):
+            check_scope = check_scope.parent
+        if arr_dim == 1:
+            match assign_op:
+                case "=":
+                    check_scope.syms[arr_name]["value"][idx1_val] = value
+                case "+=":
+                    check_scope.syms[arr_name]["value"][idx1_val] += value
+                case "-=":
+                    check_scope.syms[arr_name]["value"][idx1_val] -= value
+                case "*=":
+                    check_scope.syms[arr_name]["value"][idx1_val] *= value
+                case "/=":
+                    check_scope.syms[arr_name]["value"][idx1_val] /= value
+                case "%=":
+                    if value == 0:
+                        self.logError("Modulo by 0 is not allowed.", val_err_n)
+                    if arr_symbol["dtype"][1] not in ["int", "long"] or value_type[1] not in ["int", "long"]:
+                        self.logError("Type mismatch for arithmetic expression, modulo operation only supports whole numbers (int, long)", val_err_n)
+                    if arr_symbol["dtype"][1] == "int" and value_type[1] == "long":
+                        self.logError(f"Type mismatch for arithmetic expression, expected numeric value (int, long) for both operands, but got {arr_symbol["dtype"]} and {value_type}.", val_err_n)
+                    check_scope.syms[arr_name]["value"][idx1_val] %= value
+        else:
+            match assign_op:
+                case "=":
+                    check_scope.syms[arr_name]["value"][idx1_val][idx2_val] = value
+                case "+=":
+                    check_scope.syms[arr_name]["value"][idx1_val][idx2_val] += value
+                case "-=":
+                    check_scope.syms[arr_name]["value"][idx1_val][idx2_val] -= value
+                case "*=":
+                    check_scope.syms[arr_name]["value"][idx1_val][idx2_val] *= value
+                case "/=":
+                    check_scope.syms[arr_name]["value"][idx1_val][idx2_val] /= value
+                case "%=":
+                    if value == 0:
+                        self.logError("Modulo by 0 is not allowed.", val_err_n)
+                    if arr_symbol["dtype"][1] not in ["int", "long"] or value_type[1] not in ["int", "long"]:
+                        self.logError("Type mismatch for arithmetic expression, modulo operation only supports whole numbers (int, long)", val_err_n)
+                    if arr_symbol["dtype"][1] == "int" and value_type[1] == "long":
+                        self.logError(f"Type mismatch for arithmetic expression, expected numeric value (int, long) for both operands, but got {arr_symbol["dtype"]} and {value_type}.", val_err_n)
+                    check_scope.syms[arr_name]["value"][idx1_val][idx2_val] %= value
 
 
     def visit_node_assign_stmt_object_att(self,node):
@@ -942,6 +1008,7 @@ class Runtime:
         obj_name = node.class_att_n.obj_id_n.id_t["tokenName"]
         att_name = node.class_att_n.att_id_n.id_t["tokenName"]
         value = node.value_n
+        assign_op = node.op_t["tokenName"] 
         print(f"\nOBJ INFO: {self.curr_scope.get(obj_name)} \n{obj_name}")
         att_info = self.curr_scope.get(obj_name)["obj_info"].get(att_name)
         #print(f"!!!!!!!!!!!!!found att_info for '{att_name}' in '{obj_name}': {att_info}")   
@@ -966,7 +1033,30 @@ class Runtime:
         #    self.logError(f"Type Mismatch: expected '{dtype}' for attribute '{att_name}' but found '{val_type[1]}'", node.class_att_n.att_id_n)
 
         # self.curr_scope.set(att_name, val, dtype=dtype) #for code gen na e2 ryt TODO
-        att_info["value"] = val
+        #att_info["value"] = val
+
+        check_scope = self.curr_scope
+        while not check_scope.get(att_name, False):
+            check_scope = check_scope.parent
+        match assign_op:
+            case "=":
+                check_scope.syms[att_name]["value"] += value
+            case "+=":
+                check_scope.syms[att_name]["value"] += value
+            case "-=":
+                check_scope.syms[att_name]["value"] -= value
+            case "*=":
+                check_scope.syms[att_name]["value"] *= value
+            case "/=":
+                check_scope.syms[att_name]["value"] /= value
+            case "%=":
+                if value == 0:
+                    self.logError("Modulo by 0 is not allowed.", err_n)
+                if att_info["dtype"][1] not in ["int", "long"] or val_type[1] not in ["int", "long"]:
+                    self.logError("Type mismatch for arithmetic expression, modulo operation only supports whole numbers (int, long)", err_n)
+                if att_info["dtype"][1] == "int" and val_type[1] == "long":
+                    self.logError(f"Type mismatch for arithmetic expression, expected numeric value (int, long) for both operands, but got {att_info["dtype"]} and {val_type}.", err_n)
+                check_scope.syms[att_name]["value"] %= value
 
         print(f"\n(semantic)(dbg) EXITED node_assign_stmt_object_att!! New local object '{obj_name}' info: {self.curr_scope.get(obj_name)}")
 
