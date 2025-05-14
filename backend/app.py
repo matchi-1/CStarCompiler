77df3e0bf28df266addb1923ebd0680387c3a034
Michelle Tolentino <misheruvynx@gmail.com>
2025-05-14 17:32:49 +0800

    cleaned and added comments to app.py

--- backend/app.py
+++ backend/app.py
@@ -1,34 +1,26 @@
-# <> ------------------------------------- | WEBSOCKET TEST | AA
+
 import eventlet
 import os
 eventlet.monkey_patch()
 from flask_socketio import SocketIO
-#from inoutTest import run_loop, wait_flag_container
+# from inoutTest import run_loop, wait_flag_container   # input output test with websocket
 from runtime import setup_runtime
-
-# <> ------------------------------------- | WEBSOCKET TEST | AA
-
 from flask import Flask, json, jsonify, request
 from flask_cors import CORS
-import lexical_analyzer, syntax_analyzer, semantic_analyzer, runtime
+import lexical_analyzer, syntax_analyzer, semantic_analyzer, runtime   # import files of the analyzers + runtime
 
 app = Flask(__name__)
 CORS(app)  # cross-origin requests
 
 
-# <> ------------------------------------- | WEBSOCKET TEST | BB
+# <> ------------------------------------- | WEBSOCKET TEST CONFIG
 app.config["SECRET_KEY"] = "secret"
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 # Inject socketio to runtime
 setup_runtime(socketio)
 
-# <> ------------------------------------- | WEBSOCKET TEST | BB
+# <> ------------------------------------- | WEBSOCKET TEST CONFIG
 
-    
-#---INSTANTIATE LEXER---
-lexer = lexical_analyzer.LexicalAnalyzer()
-#--GLOBAL LISTS--
-tokens = []
 
 #---FLASK ROUTES---
 @app.route('/api/hello', methods=['GET'])
@@ -38,41 +30,45 @@ def hello():
 
 @app.route('/api/compile', methods=['POST'])
 def compile_code():
-    global tokens
-    # empty out token list every time lexer is called
-    tokens.clear()
-    data = request.json
-    code = data.get('code', '')
+    tokens = []  # holds tokens to be sent back to frontend
+    tokens.clear() # empty out token list every time lexer is called
+    data = request.json   # gets json payload from frontend, becomes a dictionary [code : ""]
+    code = data.get('code', '')  # extracts the code string from the dictionary, defaulting to an empty string if "code" is not present.
     code += '\n'
-    
-    errors = []
-    lexer_results = lexer.scan(code)  # returns [tokens, errors]
-    tokens, lexerrors = lexer_results  # unpack 
-    errors += lexerrors  # add lexer errors to the list
+    errors = []  # list of errors
 
-    # calls syntax & seman analyzer
     try:
-        syn_analyzer = syntax_analyzer.SyntaxAnalyzer(tokens)
-        parseErrs, parseTree = syn_analyzer.parse()
-        errors += parseErrs  # comment out to just test for lexer
+        # --- LEXICAL ANALYZER ---
+        lexer = lexical_analyzer.LexicalAnalyzer()  # instantiate lexical analyzer
+        lexer_results = lexer.scan(code)  # returns [tokens, errors]  -- list of tokens and errors
+        tokens, lexerrors = lexer_results  # unpack 
+        errors += lexerrors  # add lexer errors to the list
+        
+        #--- SYNTAX ANALYZER ---
+        syn_analyzer = syntax_analyzer.SyntaxAnalyzer(tokens)  # instantiate syntax analyzer
+        parseErrs, parseTree = syn_analyzer.parse()  # returns list of errors and parse tree
+        errors += parseErrs  # add syntax errors to list of errors
 
-        if "Parsing completed successfully. No Syntax Errors found." in parseErrs and not lexerrors:   # parsing success
-            seman_analyzer = semantic_analyzer.SemanticAnalyzer()
-            semanErrs = seman_analyzer.interpret(parseTree)  # comment/uncomment for testing
-            errors += semanErrs
+        # syntax parsing success
+        if "Parsing completed successfully. No Syntax Errors found." in parseErrs and not lexerrors: 
+            #-- SEMANTIC ANALYZER ---  
+            seman_analyzer = semantic_analyzer.SemanticAnalyzer() # instantiate semantic analyzer
+            semanErrs = seman_analyzer.interpret(parseTree)  # pass parse tree to semantic analyzer. returns list of semantic errors
+            errors += semanErrs  # add semantic errors to list of errors
 
+            # remove parsing success message since there's a semantic error
             if "Semantic analysis completed successfully. No Semantic Errors found." not in semanErrs:
-                # remove parsing success message since there's a semantic error
                 errors.remove("Parsing completed successfully. No Syntax Errors found.")
-            else:     # semantic success
-                runtime_res = runtime.Runtime()
-                runtimeErrs = runtime_res.interpret(parseTree)
-                # errors += runtimeErrs
-
+            
+            # semantic parsing success
+            else:   
+                runtime_res = runtime.Runtime()  # instantiate runtime
+                runtimeErrs = runtime_res.interpret(parseTree)  # pass parse tree to runtime to go over the code and execute them. returns list of runtime errors
+                
     except SyntaxError as e:
         print(e)
 
-    # handle this better next time and the ones above ^
+    # remove all immediate flag errors [NOTE: handle this better next time and the ones above ^]
     messages_to_remove = [
         "Parsing completed successfully. No Syntax Errors found.",
         "Semantic analysis completed successfully. No Semantic Errors found.",
